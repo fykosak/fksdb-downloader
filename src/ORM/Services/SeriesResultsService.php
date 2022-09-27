7f42296924c2a10164cc6f7e175da79c75b0ac6a
Michal Mišo Červeňák <miso@fykos.cz>
2022-09-27 18:33:45 +0200

    update downloader core

--- src/ORM/Services/SeriesResultsService.php
+++ src/ORM/Services/SeriesResultsService.php
@@ -1,8 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Fykosak\NetteFKSDBDownloader\ORM\Services;
 
-class SeriesResultsService
-{
+use Fykosak\FKSDBDownloaderCore\Requests\Request;
+use Fykosak\FKSDBDownloaderCore\Requests\SeriesResultsRequest;
 
+class SeriesResultsService extends AbstractJSONService
+{
+    protected function getRequest(int $contestId, int $year, int $series): Request
+    {
+        return new SeriesResultsRequest($contestId, $year, $series);
+    }
 }
